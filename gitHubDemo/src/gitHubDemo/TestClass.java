8be16e72f4eccfa5f0a0c6949250e3d8a2c99203
Ashish <patel.ashish.b@outlook.com>
2019-02-19 19:57:56 -0500

    This is the initial project commit to local repository
    Change-Id: I8f24ded34699af3d2a9c85079fd8e5730379e5d8
    Signed-off-by: Ashish <patel.ashish.b@outlook.com>

---
+++ gitHubDemo/src/gitHubDemo/TestClass.java
@@ -0,0 +1,12 @@
+package gitHubDemo;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("The git Demo Project");
+
+	}
+
+}
